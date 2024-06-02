ec04a5bb5ed45e185565679263680466788c09ba
Leandro Campos <camposweb@gmail.com>
2024-06-02 18:24:18 -0300

    new variables

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
-        //float value = scanner.nextFloat();
         GetExchange getExchange = new GetExchange();
+        Exchange convertMoney;
 
         Locale ptBr = new Locale("pt", "BR");
         Locale enUs = new Locale("en", "US");
@@ -22,6 +22,17 @@ public class Main {
         NumberFormat brlFormat = NumberFormat.getCurrencyInstance(ptBr);
         NumberFormat usFormat = NumberFormat.getCurrencyInstance(enUs);
         NumberFormat euroFormat = NumberFormat.getCurrencyInstance(euro);
+
+        String lastDate = "";
+        String nextDate = "";
+        DateTimeFormatter rfcFormattter = DateTimeFormatter.RFC_1123_DATE_TIME;
+        LocalDateTime lastDateTime;
+        LocalDateTime nextDateTime;
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("EEEE dd/MM/yyyy HH:mm:ss", ptBr);
+        String lastUpdate;
+        String nextUpdate;
+
+
 
 
         boolean state = false;
@@ -49,18 +60,20 @@ public class Main {
                     System.out.println("");
                     System.out.println("Dados da conversão");
                     System.out.println("--------------------------");
-                    Exchange usdBrl = getExchange.getConversion("USD", "BRL", dolarBrl);
+                    convertMoney = getExchange.getConversion("USD", "BRL", dolarBrl);
                     System.out.println("Valor base: " + usFormat.format(dolarBrl));
-                    System.out.println(brlFormat.format(usdBrl.conversion_result()));
+                    System.out.println(brlFormat.format(convertMoney.conversion_result()));
 
-                    String lastDate = usdBrl.time_last_update_utc();
-                    String nextDate = usdBrl.time_next_update_utc();
-                    DateTimeFormatter rfcFormattter = DateTimeFormatter.RFC_1123_DATE_TIME;
-                    LocalDateTime lastDateTime = LocalDateTime.parse(lastDate, rfcFormattter);
-                    LocalDateTime nextDateTime = LocalDateTime.parse(nextDate, rfcFormattter);
-                    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("EEEE dd/MM/yyyy HH:mm:ss", ptBr);
-                    String lastUpdate = lastDateTime.format(outputFormatter);
-                    String nextUpdate = nextDateTime.format(outputFormatter);
+                    lastDate = convertMoney.time_last_update_utc();
+                    nextDate = convertMoney.time_next_update_utc();
+
+                    lastDateTime = LocalDateTime.parse(lastDate, rfcFormattter);
+                    lastUpdate = lastDateTime.format(outputFormatter);
+
+                    nextDateTime = LocalDateTime.parse(nextDate, rfcFormattter);
+                    nextUpdate = nextDateTime.format(outputFormatter);
+
+
                     System.out.println("Última atualização: " + lastUpdate);
                     System.out.println("Próxima atualização: " + nextUpdate);
                     System.out.println("");
@@ -70,6 +83,7 @@ public class Main {
                 case 2:
                     System.out.println("Digite o valor para conversão: ");
                     float brlDolar = scanner.nextFloat();
+                    convertMoney = getExchange.getConversion("BRL", "USD", brlDolar);
                     Exchange realDolar = getExchange.getConversion("BRL", "USD", brlDolar);
                     System.out.println("Valor base: " + brlFormat.format(brlDolar));
                     System.out.println(usFormat.format(realDolar.conversion_result()));
